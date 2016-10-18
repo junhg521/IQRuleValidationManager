0230f5e75f58bae8613d8b8aa5a01498d4dfdd53
wujun60 <wujun60@wanda.cn>
2016-10-18 10:35:57 +0800

    添加过滤系统类的方法

---
+++ IQRuleValidationManager/IQRuleExpression/Category/NSString+EscapePrefix.h
@@ -0,0 +1,15 @@
+//
+//  NSString+EscapePrefix.h
+//  IQRuleValidationManager
+//
+//  Created by Jun on 16/10/18.
+//  Copyright © 2016年 JunHg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (EscapePrefix)
+
+- (BOOL)escapeWithClassPrefix:(NSString *)prefix;
+
+@end
--- IQRuleValidationManager/IQRuleExpression/IQSwizzleUtils.h
+++ IQRuleValidationManager/IQRuleExpression/IQSwizzleUtils.h
@@ -11,7 +11,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface IQSwizzleUtils : NSObject
 /**
- *  @brief  替换所有遵循protocolName协议的方法, 去除对系统类型方法进行替换（如：UI,_UI开头的系统类），所以工程命令时请不要使用改
+ *  @brief  替换所有遵循protocolName协议的方法, 去除对系统类型方法进行替换（如：UI,_UI, CN, AB开头的系统类），所以工程命令时请不要使用改
  *  类型的开头
  *
  *  @param protocolName     协议名
@@ -20,6 +20,8 @@ NS_ASSUME_NONNULL_BEGIN
  *  @param swizzledSelector 替换的selector
  */
 + (void)lookforSwizzledProtocolName:( const char * _Nonnull )protocolName originalSelector:(_Nonnull SEL)originalSelector  swizzledClass:(_Nonnull Class)swizzledClass swizzledSelector:(_Nonnull SEL) swizzledSelector;
+
+
 
 @end
 
